ad7a866d8faa0bfb346e5038fe88b1793bf418a2
Craig <craig@ubuntu-desktop.(none)>
2014-03-02 10:38:01 -0800

    Changed file into modules

--- main.py
+++ main.py
@@ -2,297 +2,31 @@ import pygame, sys, random
 import start_screen
 from pygame.locals import *
 
+import swarm
+from swarm import Ground as Ground
+from swarm import Plant as Plant
+from swarm import Player as Player
+from swarm import Bullet as Bullet
+from swarm import Controls as Controls
+
 try:
     import android
 except ImportError:
     android = None
 
-class Ground(pygame.sprite.Sprite):
-    def __init__(self):
-        self.image = pygame.image.load("img/grass.png").convert()
-        self.rect = self.image.get_rect()
-        
-class Plant(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("img/treenarrow.png").convert_alpha()
-        self.rect = self.image.get_rect()
-        
-class Player(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("img/blue_boy_small.png").convert_alpha()
-        self.rect = self.image.get_rect(center = (240, 160))
         
 
 
-class Bullet(pygame.sprite.Sprite):
-    def __init__(self, direction, character):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((6, 6))
-        #self.image.fill((255, 0, 0))
-        pygame.draw.circle(self.image, (255, 255, 255), (3, 3), 3)
-        pygame.draw.circle(self.image, (255, 0, 0), (3, 3), 1)
-        self.image.convert()
-        self.rect = self.image.get_rect()
-        self.direction = direction
-        self.speed = 7
-        if self.direction == "right":
-            self.rect.center = (character.rect.right, character.rect.centery)
-        if self.direction == "left":
-            self.rect.center = (character.rect.left, character.rect.centery)
-        if self.direction == "up":
-            self.rect.center = (character.rect.centerx, character.rect.top)
-        if self.direction == "down":
-            self.rect.center = (character.rect.centerx, character.rect.bottom)
-        if self.direction == "topLeft":
-            self.rect.center = (character.rect.left, character.rect.top)
-        if self.direction == "bottomLeft":
-            self.rect.center = (character.rect.left, character.rect.bottom)
-        if self.direction == "topRight":
-            self.rect.center = (character.rect.right, character.rect.top)
-        if self.direction == "bottomRight":
-            self.rect.center = (character.rect.right, character.rect.bottom)
-    
-        
-            
-    
-    ### movement for bullet
-    def update(self):
-      
-        if self.direction == "right":
-            if self.rect.centerx < 480:
-                self.rect.centerx += self.speed
-            else:
-                self.kill()
-        if self.direction == "left":
-            if self.rect.centerx > 0:
-                self.rect.centerx -= self.speed
-            else:
-                self.kill()
-        if self.direction == "up":
-            if self.rect.centery > 0:
-                self.rect.centery -= self.speed
-            else:
-                self.kill()
-        if self.direction == "down":
-            if self.rect.centery < 320:
-                self.rect.centery += self.speed
-            else:
-                self.kill()
-        ### Diagonal bullet firing movement
-        if self.direction == "topLeft":
-            if self.rect.centerx > 0 and self.rect.centery > 0:
-                self.rect.centerx -= 4
-                self.rect.centery -= 4
-            else:
-                self.kill()
-           
-        
-        if self.direction == "bottomLeft":
-            if self.rect.centerx > 0 and self.rect.centery < 320:
-                self.rect.centerx -= 4
-                self.rect.centery += 4
-            else:
-                self.kill()
-            
-            
-        if self.direction == "topRight":
-            if self.rect.centerx < 480 and self.rect.centery > 0:
-                self.rect.centerx += 4
-                self.rect.centery -= 4
-            else:
-                self.kill()
-            
-            
-        if self.direction == "bottomRight":
-            if self.rect.centerx < 480 and self.rect.centery < 320:
-                self.rect.centerx += 4
-                self.rect.centery += 4
+
+
+
             
         
         
         
         
 
-class Controls(pygame.sprite.Sprite):
-    def __init__(self, control_type):
-        pygame.sprite.Sprite.__init__(self)
-        if control_type == "move":
-            self.image = pygame.image.load("img/arrows_transparent.png").convert_alpha()
-            self.rect = self.image.get_rect()
-            self.rect.top = 170
-            self.rect.left = 10
-        elif control_type == "fire":
-            self.image = pygame.image.load("img/arrows_fire_directions.png").convert_alpha()
-            self.rect = self.image.get_rect()
-            self.rect.top = 170
-            self.rect.left = 320
-        self.button_size = 50
-        self.direction = "stop"
 
-        
-        
-###### creating fonts / rectangles to blit bullets # to screen
-        self.counter = 250
-        self.RED = (255, 0, 0)
-        self.LIME_GREEN = (139, 235, 103)
-        self.gameFont = pygame.font.Font("fonts/ASTONISH.TTF", 20)
-        self.bullet_counter_image = self.gameFont.render("Bullets:" + str(self.counter), True, self.RED)
-        self.bullet_counter_rect = pygame.Rect(150, 5, 60, 23)
-        
-
-    
-    def movement(self):
-        
-        pos = pygame.mouse.get_pos()
-        
-        ## creates rectangles for movement
-        self.rect_right_leftedge = self.rect.right - self.button_size
-        self.rect_right_topedge = self.rect.top + self.button_size
-        self.right_rect = pygame.Rect(self.rect_right_leftedge, self.rect_right_topedge,
-                                      self.button_size, self.button_size)
-        if self.right_rect.collidepoint(pos):
-            self.direction = "right"
-        
-        self.rect_left_leftedge = self.rect.left 
-        self.rect_left_topedge = self.rect.top + self.button_size
-        self.left_rect = pygame.Rect(self.rect_left_leftedge, self.rect_left_topedge,
-                                     self.button_size, self.button_size)
-        if self.left_rect.collidepoint(pos):
-            self.direction = "left"
-            
-        self.rect_down_leftedge = self.rect.left + self.button_size
-        self.rect_down_topedge = self.rect.top + self.button_size + self.button_size
-        self.down_rect = pygame.Rect(self.rect_down_leftedge, self.rect_down_topedge,
-                                     self.button_size, self.button_size)
-        if self.down_rect.collidepoint(pos):
-            self.direction = "down"
-            
-        self.rect_up_leftedge = self.rect.left + self.button_size
-        self.rect_up_topedge = self.rect.top 
-        self.up_rect = pygame.Rect(self.rect_up_leftedge, self.rect_up_topedge,
-                                     self.button_size, self.button_size)
-        if self.up_rect.collidepoint(pos):
-            self.direction = "up"
-            
-        self.rect_stop_leftedge = self.rect.left + self.button_size
-        self.rect_stop_topedge = self.rect.top + self.button_size
-        self.stop_rect = pygame.Rect(self.rect_stop_leftedge, self.rect_stop_topedge,
-                                     self.button_size, self.button_size)
-        if self.stop_rect.collidepoint(pos):
-            self.direction = "stop"
-            
-        return(self.direction)
-        
-    
-    
-    
-    def firing(self, character, bullet_group, windowSurface):
-        
-        pos = pygame.mouse.get_pos()
-        
-        
-        ### creates the rectangles for firing bullets and adds them to the list
-        self.rect_right_leftedge = self.rect.right - self.button_size
-        self.rect_right_topedge = self.rect.top + self.button_size
-        self.right_rect = pygame.Rect(self.rect_right_leftedge, self.rect_right_topedge,
-                                      self.button_size, self.button_size)
-    
-######## checking for number of bullets    
-        if self.right_rect.collidepoint(pos) and self.counter > 0:
-            direction = "right"
-            bullet = Bullet(direction, character)
-            bullet_group.add(bullet)
-            self.counter = self.counter - 1
-            #print (self.counter)
-        
-        self.rect_left_leftedge = self.rect.left 
-        self.rect_left_topedge = self.rect.top + self.button_size
-        self.left_rect = pygame.Rect(self.rect_left_leftedge, self.rect_left_topedge,
-                                     self.button_size, self.button_size)
-        
-        if self.left_rect.collidepoint(pos) and self.counter > 0:
-            direction = "left"
-            bullet = Bullet(direction, character)
-            bullet_group.add(bullet)
-            self.counter = self.counter - 1
-            
-        self.rect_down_leftedge = self.rect.left + self.button_size
-        self.rect_down_topedge = self.rect.top + self.button_size + self.button_size
-        self.down_rect = pygame.Rect(self.rect_down_leftedge, self.rect_down_topedge,
-                                     self.button_size, self.button_size)
-        
-        if self.down_rect.collidepoint(pos) and self.counter > 0:
-            direction = "down"
-            bullet = Bullet(direction, character)
-            bullet_group.add(bullet)
-            self.counter = self.counter - 1
-            
-        self.rect_up_leftedge = self.rect.left + self.button_size
-        self.rect_up_topedge = self.rect.top 
-        self.up_rect = pygame.Rect(self.rect_up_leftedge, self.rect_up_topedge,
-                                     self.button_size, self.button_size)
-        
-        if self.up_rect.collidepoint(pos) and self.counter > 0:
-            direction = "up"
-            bullet = Bullet(direction, character)
-            bullet_group.add(bullet)
-            self.counter = self.counter - 1
-        
-        
-        
-        ### directions for diagonal shooting controls    
-        self.rect_TopLeftDiag_leftedge = self.rect.left
-        self.rect_TopLeftDiag_topedge = self.rect.top
-        self.TopLeftDiag_rect = pygame.Rect(self.rect_TopLeftDiag_leftedge, self.rect_TopLeftDiag_topedge,
-                                            self.button_size, self.button_size)
-        
-        if self.TopLeftDiag_rect.collidepoint(pos) and self.counter > 0:
-            direction = "topLeft"
-            bullet = Bullet(direction, character)
-            bullet_group.add(bullet)
-            self.counter = self.counter - 1
-            
-        self.rect_BottomLeftDiag_leftedge = self.rect.left
-        self.rect_BottomLeftDiag_topedge = self.rect.top + self.button_size + self.button_size
-        self.BottomLeftDiag_rect = pygame.Rect(self.rect_BottomLeftDiag_leftedge, self.rect_BottomLeftDiag_topedge,
-                                               self.button_size, self.button_size)
-        
-        if self.BottomLeftDiag_rect.collidepoint(pos) and self.counter > 0:
-            direction = "bottomLeft"
-            bullet = Bullet(direction, character)
-            bullet_group.add(bullet)
-            self.counter = self.counter - 1
-            
-        self.rect_TopRightDiag_leftedge = self.rect.right - self.button_size
-        self.rect_TopRightDiag_topedge = self.rect.top
-        self.TopRightDiag_rect = pygame.Rect(self.rect_TopRightDiag_leftedge, self.rect_TopRightDiag_topedge,
-                                             self.button_size, self.button_size)
-        
-        if self.TopRightDiag_rect.collidepoint(pos) and self.counter > 0:
-            direction = "topRight"
-            bullet = Bullet(direction, character)
-            bullet_group.add(bullet)
-            self.counter = self.counter - 1
-            
-        self.rect_BottomRightDiag_leftedge = self.rect.right - self.button_size
-        self.rect_BottomRightDiag_topedge = self.rect.top + self.button_size + self.button_size
-        self.BottomRightDiag_rect = pygame.Rect(self.rect_BottomRightDiag_leftedge, self.rect_BottomRightDiag_topedge,
-                                                self.button_size, self.button_size)
-        
-        if self.BottomRightDiag_rect.collidepoint(pos) and self.counter > 0:
-            direction = "bottomRight"
-            bullet = Bullet(direction, character)
-            bullet_group.add(bullet)
-            self.counter = self.counter - 1
-            
-        
-#####
-      
-        
-        
-        return(bullet_group)
         
 class Monster(pygame.sprite.Sprite):
     def __init__(self):
@@ -837,7 +571,16 @@ def main():
     
     while gameOn:
 ### start screen
+        
         if timer.level == 0:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        pygame.quit()
+                        sys.exit()
             startScreen.start()
             windowSurface.blit(startScreen.start(), (0, 0))
             timer.level = startScreen.checkLevel()
@@ -850,7 +593,8 @@ def main():
                     sys.exit()
                 if event.type == KEYDOWN:
                     if event.key == K_ESCAPE:
-                        gameOn = False
+                        pygame.quit()
+                        sys.exit()
                 if event.type == MOUSEBUTTONDOWN:
                     bullet_group = fire.firing(character, bullet_group, windowSurface)
                    # print (fire.counter)
---
+++ swarm/__init__.py
@@ -0,0 +1,5 @@
+from ground import Ground
+from plant import Plant
+from player import Player
+from bullet import Bullet
+from controls import Controls
